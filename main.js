1423a13c151953a565dc8d115738a7b695c495b4
Yasuo67 <aomiii876@gmail.com>
2024-10-05 00:18:56 +0530

    added form

---
+++ main.js
@@ -0,0 +1,6 @@
+// const form = document.forms["sign-up-form"];
+// function validateForm(){
+//     if(form["password"].value != form["confirm-password"].value){
+//         return false;
+//     }
+// }
