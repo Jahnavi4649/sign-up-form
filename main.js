f3a89b482577b13c8425c3b7e6e299079a9915ae
Yasuo67 <aomiii876@gmail.com>
2024-10-05 12:16:45 +0530

    added password validation

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
-// const form = document.forms["sign-up-form"];
-// function validateForm(){
-//     if(form["password"].value != form["confirm-password"].value){
-//         return false;
-//     }
-// }
+const form = document.forms["sign-up-form"];
+const message = document.getElementById("message");
+message.style.color="red";
+message.style.fontSize ="14px";
+let validate;
+function validateForm(){
+    message.innerHTML="";
+    if(form["password"].value=="" || form["confirm-password"].value==""){
+        message.innerHTML="*Password fields cannot be empty";
+        return false;
+    }
+    if(form["password"].value != form["confirm-password"].value){
+        message.innerHTML= "*Passwords do not match";
+        return false;
+    }
+    else{
+        changeClasses();
+        return true
+    }
+}
+function changeClasses(){
+    const signUpPage = document.getElementById("signup-page");
+    const successPage = document.getElementById("success-page");
+    signUpPage.classList.add("hidden");
+    signUpPage.classList.remove("signup-page");
+    successPage.classList.remove("hidden");
+    successPage.classList.add("success-page");
+
+}
